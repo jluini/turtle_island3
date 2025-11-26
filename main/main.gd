0f88f203ee71817e8bcbfe268cf886d895c16655
jluini <jluini@gmail.com>
2025-11-26 16:45:55 -0300

    States and state label

--- main/main.gd
+++ main/main.gd
@@ -1,7 +1,25 @@
 extends Node2D
 
+enum State {
+	NOTHING,
+	INITIAL
+}
+
+var state : State = State.NOTHING:
+	set(new_state):
+		state = new_state
+		$ui.get_node("%state_label").text = State.find_key(new_state)
+		
+
 func _ready() -> void:
 	get_window().position = Vector2(420, 0)
+	state = State.INITIAL
+
+func _set_state(new_state:State) -> void:
+	state = new_state
+	
+
+##
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_left"):
